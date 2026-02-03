d9c9202f9e93ddee73d0066594f83f66aa06fb69
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-02-03 08:07:23 -0700

    Removed unused quest scripts and entities to declutter the project.

--- SampleQuests/MyThirdQuest/MyThirdQuest.lua
+++
@@ -1,5 +0,0 @@
-function Main(Quest)
-    -- You now call Log as a method of the Quest object
-    Quest:Log("This script now works with the new system!")
-	Quest:ShowMessage("It's the third quest", 5.0)
-end
